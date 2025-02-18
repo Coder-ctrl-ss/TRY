ea683ced0a7080d814335328519965fded179d80
Coder-ctrl-ss <gagankumar005.up@gmail.com>
2025-02-18 15:00:52 +0530

    simple program in JS

---
+++ 555.js
@@ -0,0 +1,27 @@
+const prompt=require("prompt-sync")();
+function h(){
+    console.log("hello");
+}
+function h1(){
+    console.log("hi");
+}
+function h12(){
+    console.log("helooooooooo");
+}
+
+h12();
+h1();
+
+
+// let sum=0
+// L=Number(prompt("Enter the Number"));
+// ap(L);
+// for(let i=1;i<=L;i++){
+//     console.log(i);
+// }
+// function ap(L){
+//     for(let i=1;i<=L;i=i+2){
+//         sum+=i;
+//     }
+// }
+// console.log("sum of all even number is",sum);
